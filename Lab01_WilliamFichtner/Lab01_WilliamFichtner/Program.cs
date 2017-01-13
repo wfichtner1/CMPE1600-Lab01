db7643550c9409b2cf1bd6013627ff3d1de9a396
William Fichtner <william_25_35@hotmail.com>
2017-01-12 18:02:10 -0700

    finished

--- Lab01_WilliamFichtner/Lab01_WilliamFichtner/Program.cs
+++ Lab01_WilliamFichtner/Lab01_WilliamFichtner/Program.cs
@@ -6,47 +6,63 @@ using System.Threading.Tasks;
 using System.Drawing;
 using GDIDrawer;
 
+//Program:      Lab01_WilliamFichtner
+//Description:  Takes a monetary value given by  user and
+//              sorts it into different denominations
+//Lab:          1
+//Date:         January 11.2017
+//Author:       William Fichtner
+//Class:        CMPE 1600
+//Instructor:   JD Silver
 
 namespace Lab01_WilliamFichtner
 {
     class Program
     {
-        
-        
 
         static void Main(string[] args)
-        {
-            
-            double[] denomArr = new double[9];
-            int index = 0;
-            double userInput = 0;
-            string input = null;
-            double fifties = 0;            
-            double twenties = 0;
-            double tens = 0;
-            double fives = 0;
-            double toonies = 0;
-            double loonies = 0;
-            double quarters = 0;
-            double dimes = 0;
-            double nickels = 0;
+        {            
+            double[] denomArr = new double[9];      //An array that holds the amount of each denomination for the GDI drawer
+            double userInput = 0;                   //User input that has been converted to a double
+            bool proceed = true;                    //Check for input loop
+            string total;                           //Used to pass the user value as a string to the draw method 
+            string input = null;                    //Initial input given by user
+            double fifties = 0;                     //Holds the amount of $50 bills
+            double twenties = 0;                    //Holds the amount of $20 bills
+            double tens = 0;                        //Holds the amount of $10 bills
+            double fives = 0;                       //Holds the amount of $5 bills
+            double toonies = 0;                     //Holds the amount of toonies
+            double loonies = 0;                     //Holds the amount of loonies
+            double quarters = 0;                    //Holds the amount of quarters
+            double dimes = 0;                       //Holds the amount of dimes
+            double nickels = 0;                     //Holds the amount of nickels
 
             //Asks for user input and ensures the input is in proper format
-            Console.Write("Please enter a dollar amount:");
-            foreach(char c in Console.ReadLine())
+            do
             {
-                if (char.IsDigit(c) || c == '.')
+                Console.Write("Please enter a dollar amount: ");
+                proceed = true;
+
+                //Checks each character in the user input and ensures it is a digit, so symbols and letters are disallowed
+                foreach (char c in Console.ReadLine())
                 {
-                    input += c;
+                    if (char.IsDigit(c) || c == '.')
+                    {
+                        input += c;
+                    }
                 }
-            }
-            if(double.TryParse(input, out userInput) == false)
-            {
-                Console.WriteLine("Please enter a valid dollar amount: ");
-            }
+
+                total = input;
+
+                //converts input to double, and asks for new input if unable
+                if (double.TryParse(input, out userInput) == false)
+                {
+                    Console.WriteLine("Please enter a valid dollar amount: ");
+                    proceed = false;
+                }
+            } while (proceed == false);
 
             userInput = Math.Round(userInput / 0.05) * 0.05;
-
 
             Console.WriteLine("User entry interpreted and rounded to {0:C}", userInput);
 
@@ -108,12 +124,12 @@ namespace Lab01_WilliamFichtner
             userInput = userInput - (nickels * 0.05);
             denomArr[8] = nickels;
 
-            Draw(denomArr);
+            Draw(denomArr, total);
             
 
             Console.ReadLine();
         }
-
+        //Calculates the amount of each denomination in the input given by user
         private static double CurrencyCalc(double userInput, double currencyUnit)
         {
             double amount;
@@ -124,14 +140,20 @@ namespace Lab01_WilliamFichtner
 
             return total;
         }
+        //Writes the amount of each denomination in the console
         private static void Display(double amount, string input)
         {
             Console.WriteLine("{0}: {1}", input, amount);
         }
-        private static void Draw(double[] arr)
+        //Draws the denominations on the GDI canmvas
+        private static void Draw(double[] arr, string total)
         {
             CDrawer canvas = new CDrawer();
+
+            canvas.AddText("$" + total, 20, 350, 0, 150, 40, Color.Gold); 
             
+            //This series checks if there is an amount of each denomination
+            //and then chooses to draw it or not
             if(arr[0] > 0)
             {
                 canvas.AddRectangle(50, 30, 300, 130, Color.Red);
@@ -156,6 +178,27 @@ namespace Lab01_WilliamFichtner
             {
                 canvas.AddEllipse(500, 30, 100, 100, Color.Silver);
                 canvas.AddCenteredEllipse(550, 80, 80, 80, Color.Gold);
+                canvas.AddText("2 X " + arr[4].ToString(), 20, 510, 40, 80, 80, Color.White);
+            }
+            if (arr[5] > 0)
+            {
+                canvas.AddEllipse(505, 140, 90, 90, Color.Gold);
+                canvas.AddText("1 X " + arr[5].ToString(), 20, 510, 145, 80, 80, Color.White);
+            }
+            if (arr[6] > 0)
+            {
+                canvas.AddEllipse(505, 250, 80, 80, Color.Silver);
+                canvas.AddText("0.25 X " + arr[6].ToString(), 15, 500, 245, 90, 90, Color.White);
+            }
+            if (arr[7] > 0)
+            {
+                canvas.AddEllipse(515, 360, 60, 60, Color.Silver);
+                canvas.AddText("0.10 X " + arr[7].ToString(), 15, 500, 345, 90, 90, Color.White);
+            }
+            if (arr[8] > 0)
+            {
+                canvas.AddEllipse(505, 470, 70, 70, Color.Silver);
+                canvas.AddText("0.05 X " + arr[8].ToString(), 15, 500, 460, 90, 90, Color.White);
             }
 
         }
